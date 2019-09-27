39843b5f4f848d66132d8b333c91c997f7facdfd
Marvin Caragay <mrvncaragay@gmail.com>
2019-09-27 16:14:15 -0700

    convert react to jsx

--- react/ADOPT-ME/dist/App.d36a57b6.js
+++ react/ADOPT-ME/dist/App.d36a57b6.js
@@ -32004,7 +32004,7 @@ var Pet = function Pet(_ref) {
   var name = _ref.name,
       animal = _ref.animal,
       breed = _ref.breed;
-  return _react.default.createElement('div', {}, [_react.default.createElement('h1', {}, name), _react.default.createElement('h2', {}, animal), _react.default.createElement('h2', {}, breed)]);
+  return _react.default.createElement("div", null, _react.default.createElement("h1", null, name), _react.default.createElement("h2", null, animal), _react.default.createElement("h2", null, breed));
 };
 
 var _default = Pet;
@@ -32021,22 +32021,22 @@ var _Pet = _interopRequireDefault(require("./Pet"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var App = function App() {
-  return _react.default.createElement('div', {}, [_react.default.createElement('h1', {}, 'Adopt Me!'), _react.default.createElement(_Pet.default, {
-    name: 'Luna',
-    animal: 'Dog',
-    breed: 'Havanese'
+  return _react.default.createElement("div", null, _react.default.createElement("h1", null, "Adopt Me!"), _react.default.createElement(_Pet.default, {
+    nane: "Luna",
+    animal: "Dog",
+    breed: "Havanese"
   }), _react.default.createElement(_Pet.default, {
-    name: 'Pepper',
-    animal: 'Bird',
-    breed: 'Cockatiel'
+    nane: "Pepper",
+    animal: "Bird",
+    breed: "Cockatiel"
   }), _react.default.createElement(_Pet.default, {
-    name: 'Doink',
-    animal: 'Cat',
-    breed: 'Mixed'
-  })]);
+    nane: "Doink",
+    animal: "Cat",
+    breed: "Mixed"
+  }));
 };
 
-(0, _reactDom.render)(_react.default.createElement(App), document.getElementById('root'));
+(0, _reactDom.render)(_react.default.createElement(App, null), document.getElementById('root'));
 },{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","./Pet":"Pet.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -32065,7 +32065,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58194" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58533" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
